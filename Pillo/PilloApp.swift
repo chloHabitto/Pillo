8488456b7e3dda9ba656f68861c0357a22c182a1
chloHabitto <chloe@habitto.nl>
2025-12-10 18:27:05 +0100

    Initial Commit

---
+++ Pillo/PilloApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilloApp.swift
+//  Pillo
+//
+//  Created by Chloe Lee on 2025-12-10.
+//
+
+import SwiftUI
+
+@main
+struct PilloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PilloTests/PilloTests.swift
@@ -0,0 +1,16 @@
+//
+//  PilloTests.swift
+//  PilloTests
+//
+//  Created by Chloe Lee on 2025-12-10.
+//
+
+import Testing
+
+struct PilloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
